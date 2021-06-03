d0369f213593b27c4719393d8669a73603e04ce6
rurewa <rurewa@gmail.com>
2021-06-03 10:47:26 +0300

    Update cal.py v1.2: add check correct inter number end operator

--- cal.py
+++ cal.py
@@ -1,26 +1,56 @@
 # Мой первый калькулятор на Python v1.2
 # Осуществлена проверка корректности ввода знака оператора
 print("Здравуствуйте! Это калькулятор.")
-print("Введите по очереди два числа")
-print("Введите первое число: ")
-first_num = float(input())
+print("Введите по очереди: первое число, оператор и второе число, нажимая Enter")
+
+first_num = input("Введите первое число: ")
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Проверка корректности ввода первого числа
+is_num = True 
+try:
+    float(first_num)
+except ValueError:
+    is_num = False
+if is_num == True:
+    first_num = float(first_num)
+    print("Вы ввели: ", first_num)
+else:
+    print("Вы ввели некорректные данные")
+    exit(0) # Завершение программы
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 print("Введите оператор: +, -, *, /")
 operator = str(input())
-print("Введите второе число")
-second_num = float(input())
-
+# Проверка правильности ввода математического оператора
 if operator == '+' or operator == '-' or operator == '*' or operator == '/':
-    if operator == '+':
-        result = first_num + second_num
-        print("Результат сложения", result)
-    elif operator == '-':
-        result = first_num - second_num
-        print("Результат вычитания: ", result)
-    elif operator == '*':
-        result = first_num * second_num
-        print("Результат произведени: ", result)
-    else:
-        result = first_num / second_num
-        print("Результат деления: ", result)
+    print("Выввели:", operator)
 else:
-    print("Выввели неправильный оператор!")     
+    print("Вы ввели неправильный оператор!")
+    exit(0)
+
+second_num = input("Введите второе число: ")
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Проверка корректности ввода числа
+is_num = True 
+try:
+    float(second_num)
+except ValueError:
+    is_num = False
+if is_num == True:
+    second_num = float(second_num)
+    print("Вы ввели: ", second_num)
+else:
+    print("Вы ввели некорректные данные")
+    exit(0)
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+if operator == '+':
+    result = first_num + second_num
+    print("Результат сложения", result)
+elif operator == '-':
+    result = first_num - second_num
+    print("Результат вычитания: ", result)
+elif operator == '*':
+    result = first_num * second_num
+    print("Результат произведени: ", result)
+else:
+    result = first_num / second_num
+    print("Результат деления: ", result)
