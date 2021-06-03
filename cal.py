ddc29b8dc6791e64a7e3773bc8d02b244c3e1a4e
rurewa <rurewa@gmail.com>
2021-06-03 15:19:56 +0300

    Update cal.py: fix zero result

--- cal.py
+++ cal.py
@@ -1,7 +1,7 @@
-# Мой первый калькулятор на Python v1.2
+# Мой первый калькулятор на Python v1.3
 # Осуществлена проверка корректности ввода знака оператора
 print("Здравуствуйте! Это калькулятор.")
-print("Введите по очереди: первое число, оператор и второе число, нажимая Enter")
+print("Введите по очереди: первое число, оператоб, второе число, по очереди, нажимая Enter")
 
 first_num = input("Введите первое число: ")
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -15,21 +15,21 @@ if is_num == True:
     first_num = float(first_num)
     print("Вы ввели: ", first_num)
 else:
-    print("Вы ввели некорректные данные")
+    print("Это не число!")
     exit(0) # Завершение программы
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 print("Введите оператор: +, -, *, /")
 operator = str(input())
 # Проверка правильности ввода математического оператора
 if operator == '+' or operator == '-' or operator == '*' or operator == '/':
-    print("Выввели:", operator)
+    print("Вы ввели:", operator)
 else:
     print("Вы ввели неправильный оператор!")
     exit(0)
 
 second_num = input("Введите второе число: ")
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# Проверка корректности ввода числа
+# Проверка корректности второго ввода числа
 is_num = True 
 try:
     float(second_num)
@@ -39,9 +39,10 @@ if is_num == True:
     second_num = float(second_num)
     print("Вы ввели: ", second_num)
 else:
-    print("Вы ввели некорректные данные")
+    print("Это не число!")
     exit(0)
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Вычисляем результат
 if operator == '+':
     result = first_num + second_num
     print("Результат сложения", result)
@@ -52,5 +53,8 @@ elif operator == '*':
     result = first_num * second_num
     print("Результат произведени: ", result)
 else:
-    result = first_num / second_num
-    print("Результат деления: ", result)
+    if second_num == 0:
+        print("На ноль делить нельзя!")
+    else:    
+        result = first_num / second_num
+        print("Результат деления: ", result)
