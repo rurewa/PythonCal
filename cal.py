1d7d68ee4a7e3239217cdc325e3e79781c6e73a4
Alex <rise.sp@gmail.com>
2021-06-07 08:43:26 +0300

    Fix merge conflict

--- cal.py
+++ cal.py
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-# Мой первый калькулятор на Python. V1.1
-print("Здравуствуйте! Это калькулятор.")
-print("Введите по очереди два числа")
-print("Введите первое число: ")
-first_num = float(input())
-print("Введите оператор: +, -, *, /")
-operator = str(input())
-print("Введите второе число")
-second_num = float(input())
-# Условие для проверки корректности ввода оператора
-if operator == '+' or operator == '-' or operator == '*' or operator == '/':
-    if operator == '+':
-        result = first_num + second_num
-        print("Результат сложения", result)
-    elif operator == '-':
-        result = first_num - second_num
-        print("Результат вычитания: ", result)
-    elif operator == '*':
-        result = first_num * second_num
-        print("Результат произведени: ", result)
-    else:
-        result = first_num / second_num
-        print("Результат деления: ", result)
-else:
-    print("Вы ввели неверный оператор!")
-=======
 # Мой первый калькулятор на Python v1.3
 # Осуществлена проверка корректности ввода знака оператора
 print("Здравуствуйте! Это калькулятор.")
@@ -85,4 +58,3 @@ else:
     else:    
         result = first_num / second_num
         print("Результат деления: ", result)
->>>>>>> ddc29b8dc6791e64a7e3773bc8d02b244c3e1a4e
