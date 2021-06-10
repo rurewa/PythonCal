845047ad863b4665bf5035c7a8e07a3433b077e2
Alex <rise.sp@gmail.com>
2021-06-10 04:28:59 +0300

    Added examples from Timofey

---
+++ tim2.py
@@ -0,0 +1,42 @@
+import operator #библеотека для замены операторов командами
+
+print("This is calculator program")
+
+print("Please enter any math problem")
+MathProblem =  (input()) # перменная в которой храниться пример
+
+Operatorsset = set(('-', '+', '=', '*', '/')) # множество для праверки евляетьсяли символ оперетором
+
+# словарь нужен для сопоствления строковых символов с командами
+Operatorsdict = {'-' : operator.sub, '+' : operator.add, '/' : operator.truediv, '*' : operator.mul}
+Numbers = [] # лист для чисел
+OperatorsList = [] # лист для операторов
+
+Start = 0 # переменная для указания начала среза который превратится в число 
+
+for i, nomer in zip (MathProblem, range(len(MathProblem))): # перебор примера для разложения его на операторы и числ
+    if(Operatorsset >= set(i)): # проверка евляется ли символ оператором
+        OperatorsList.append(i) # добавление оператора
+        try:
+            Numbers.append(float(MathProblem[Start:nomer])) # добавление числа в Numbers
+        except ValueError:
+            print('You print unacceptable char')
+            exit()
+        # проверка на недопустимые символы
+        Start = nomer + 1    
+
+try:
+    Result = Numbers[0]
+except IndexError:
+    print("You don't write operators")
+    exit(0)
+    #проверка на не написанные операторы
+for i, nomer in zip(OperatorsList, range(len(OperatorsList))): # перебор операторов   
+    try:
+        Result = Operatorsdict[i](Result, Numbers[nomer + 1]) # вычисление
+    except IndexError:
+        print("You don't write =" )
+    # проверка на ненаписание ровно
+    except KeyError:
+        print(Result)#вывод результата
+    # проверка на ошибку которая происходит если написать ровно она должна произойти для вывода результата
---
+++ tim_opt.py
@@ -0,0 +1,5 @@
+print("This is calculator program")
+
+MathProblem =  (input("Please enter any math problem"))
+
+print(eval(MathProblem))
